b32fa5972b2f05cf5e7280ceee84e8e98bf8c2d3
Ana <anatorres.atf@gmail.com>
2019-10-16 15:17:00 +0200

    funciones ok

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-function superHeroe() {
-    let genero = prompt("Indique si es hombre (h) o mujer(m)")
-    let titulos_masculino = ["Boy", "Man", "Man", "Man", "Man", "Guy", "Mr.", "Doctor" , "El", "Professor", "Captain", "Agent", "The", "The", "The", "The", "Lord", "Sgt.", "General"];
-    const titulos_femenino = ["Girl", "Woman", "Woman", "Woman", "Woman", "Gal", "Miss", "Doctor", "Professor", "Captain", "Agent", "The", "The", "The", "Sgt.", "General"];
-    let adjetivos = ["Imperial", "Amazing", "Spectular", "Dino", "Massive", "Fantastic", "Wonder", "Techno", "Electro", "Hydro", "Giant", "Super", "Incredi", "Daring", "Mega", "Tiny", "Red", "Dark", "Orange", "Screaming", "Green", "Teal", "Blue", "Golden", "Fearless", "Great", "Ameri", "Pyro", "Robo", "American", "Cyber", "Frozen"]
-    let sustantivos = ["Spider", "Laser", "Microbe", "Spectre", "Scan", "Badger", "Lighting", "Thunder", "Eagle", "Hurricane", "Storm", "Typhoon", "Fire", "Flame", "Flash", "Night", "Whirlwind", "Wind", "Dawn", "Light", "Dragon", "Wolf", "Vemon", "Cobra", "Viper", "Condor", "Stalker", "Panther", "Puma", "Shadow", "Freeze", "Night", "Hammer", "Mist", "Tulip", "Octopus", "Inferno", "Magma", "Patriot", "Stag", "Rhino", "Mole", "Sloth"]
-    
-    
-    let nombre1 = Math.round(Math.random()* titulos_masculino.length -1);   
-    let nombre2 = Math.round(Math.random()* titulos_femenino.length -1);
-    let nombre3 = Math.round(Math.random()* adjetivos.length -1);
-    let nombre4 = Math.round(Math.random()* sustantivos.length -1);
+/*Ejercicio 1:
 
-     
+Crearemos una aplicación web que consistirá en:
 
-    if (genero === "h"){
-        document.write("Mi nombre es" + titulos_masculino[nombre1] + " " + adjetivos[nombre3] + " " + sustantivos[nombre4]) 
-    }else if (genero === "m"){
-        document.write("Mi nombre es" + titulos_femenino[nombre2] + " " + adjetivos[nombre3] + " " + sustantivos[nombre4])
++ Un botón para generar nombres de superhéroes *masculinos*
++ Un botón para generar nombres de superhéroes *femeninos*
++ Un espacio en blanco para visualizar el nombre generado
 
-    }return true
+Cuando se haga click sobre uno de los botones, se generará aleatoriamente un nombre del género 
+correspondiente y se imprimirá en el espacio en blanco. 
+
+Además, dicho nombre generado se guardará en las cookies o en el localStorage del cliente.
+
+Finalmente, cuando se cargue la página, se comprobarán las cookies o el localStorage para 
+comprobar si ya hay algún nombre anteriormente generado. Si lo hay, se pintará en el espacio
+en blanco correspondiente por defecto.
+
+*IMPORTANTE* Crear un repo de git solamente para este proyecto, a parte de los otros repos de git que tenéis. */
+
+
+
+
+let titulos_masculino = ["Boy", "Man", "Man", "Man", "Man", "Guy", "Mr.", "Doctor" , "El", "Professor", "Captain", "Agent", "The", "The", "The", "The", "Lord", "Sgt.", "General"];
+const titulos_femenino = ["Girl", "Woman", "Woman", "Woman", "Woman", "Gal", "Miss", "Doctor", "Professor", "Captain", "Agent", "The", "The", "The", "Sgt.", "General"];
+let adjetivos = ["Imperial", "Amazing", "Spectular", "Dino", "Massive", "Fantastic", "Wonder", "Techno", "Electro", "Hydro", "Giant", "Super", "Incredi", "Daring", "Mega", "Tiny", "Red", "Dark", "Orange", "Screaming", "Green", "Teal", "Blue", "Golden", "Fearless", "Great", "Ameri", "Pyro", "Robo", "American", "Cyber", "Frozen"]
+let sustantivos = ["Spider", "Laser", "Microbe", "Spectre", "Scan", "Badger", "Lighting", "Thunder", "Eagle", "Hurricane", "Storm", "Typhoon", "Fire", "Flame", "Flash", "Night", "Whirlwind", "Wind", "Dawn", "Light", "Dragon", "Wolf", "Vemon", "Cobra", "Viper", "Condor", "Stalker", "Panther", "Puma", "Shadow", "Freeze", "Night", "Hammer", "Mist", "Tulip", "Octopus", "Inferno", "Magma", "Patriot", "Stag", "Rhino", "Mole", "Sloth"]
     
+let nombre1 = Math.round(Math.random()* titulos_masculino.length -1);   
+let nombre2 = Math.round(Math.random()* titulos_femenino.length -1);
+let nombre3 = Math.round(Math.random()* adjetivos.length -1);
+let nombre4 = Math.round(Math.random()* sustantivos.length -1);
+
+
+function masculino() {
+    document.querySelectorAll("input")[0].value = "Mi nombre es" + titulos_masculino[nombre1] + " " + adjetivos[nombre3] + " " + sustantivos[nombre4];
+    document.cookie = `username = ${document.querySelectorAll("input")[0].value}`;
+    return true
 }
 
-let boton12 = document.querySelectorAll("body > button")[12]
-boton12.addEventListener("click", superHeroe)
+function femenino() {
+    document.querySelectorAll("input")[0].value ="Mi nombre es" + titulos_femenino[nombre2] + " " + adjetivos[nombre3] + " " + sustantivos[nombre4];
+    document.cookie = `username = ${document.querySelectorAll("input")[0].value}`;
+    return true
+}
+
+let botonM = document.querySelectorAll("body > button")[0]
+botonM.addEventListener("click", masculino)    
+
+
+let botonF = document.querySelectorAll("body > button")[1]
+botonF.addEventListener("click", femenino)
+
+
+
+
